2619af355f093258cc463e223aa74afe7ce7b7cb
Hasan <hasanawata1985@gmail.com>
2026-04-12 11:38:33 +0300

    Adding the latest database files# Please enter the commit message for your changes. Lines starting

--- Database/01_Creating_Database_And_Tables.sql
+++ Database/01_Creating_Database_And_Tables.sql
@@ -40,7 +40,6 @@ CREATE TABLE Currencies (
 CREATE TABLE Categories (
     CategoryID INT IDENTITY(1,1) PRIMARY KEY,
     Name NVARCHAR(100) NOT NULL,
-    Description NVARCHAR(255),
     Priority INT NOT NULL
 );
 
